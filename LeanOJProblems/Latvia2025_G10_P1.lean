e6b8eaa5ae7c2a05a5c645395e9dbdcc44a58374
michailus <mihail.ab3@gmail.com>
2026-02-07 00:13:13 +0100

    Fixed empty lines

--- LeanOJProblems/Latvia2025_G10_P1.lean
+++ LeanOJProblems/Latvia2025_G10_P1.lean
@@ -1,8 +1,5 @@
 import Mathlib.Data.Real.Basic
 
-def answer : Bool := sorry
-
+def answer : Prop := sorry
 
 theorem solution : (∃ a b c d : ℤ, |a - b| + |b - c| + |c - d| + |d - a| = 2025) ↔ answer := sorry 
-  
-  
