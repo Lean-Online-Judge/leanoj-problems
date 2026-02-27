5e16a588d1c758e38dd86770d04da0ef0adcbfe3
Felix Pernegger <s59fpern@uni-bonn.de>
2026-02-27 16:39:30 +0100

    change format

--- LeanOJProblems/Austria2023P1.lean
+++ LeanOJProblems/Austria2023P1.lean
@@ -2,6 +2,6 @@ import Mathlib.Data.Real.Basic
 
 def P (f : ℝ → ℝ) (α : ℝ) := ∀ x y, f (f (x + y)) = f (x + y) + f x * f y + α * x * y
 
-def answer : Set (ℝ × (ℝ → ℝ)) := sorry
+def answer (α : ℝ) : Set (ℝ → ℝ) := sorry
 
-theorem solution (f : ℝ → ℝ) {α : ℝ} (hα : α ≠ 0) : P f α ↔ (α, f) ∈ answer := sorry
+theorem solution (f : ℝ → ℝ) {α : ℝ} (hα : α ≠ 0) : P f α ↔ f ∈ answer α := sorry
